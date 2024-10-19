07a807b363996d1bca0b197702010f15d7a42568
Manoj <manoj09633@gmail.com>
2024-10-19 13:10:25 +0530

    created supabase client

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private supabase!: SupabaseClient
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+  }
 }

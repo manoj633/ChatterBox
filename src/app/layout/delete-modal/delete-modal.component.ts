5504ff18cf845198052de76b0bd2090a252e9672
Manoj <manoj09633@gmail.com>
2024-10-21 21:58:55 +0530

    created delete confirmation modal

--- src/app/layout/delete-modal/delete-modal.component.ts
+++ src/app/layout/delete-modal/delete-modal.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { ChatService } from '../../supabase/chat.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-modal',
   standalone: true,
   imports: [],
   templateUrl: './delete-modal.component.html',
-  styleUrl: './delete-modal.component.css'
+  styleUrl: './delete-modal.component.css',
 })
 export class DeleteModalComponent {
+  private chat_service = inject(ChatService);
+  private router = inject(Router);
 
+  dismiss = signal(false);
+
+  constructor() {
+    effect(() => {
+      console.log(this.chat_service.savedChats());
+    });
+  }
+
+  deleteChat() {
+    const id = (this.chat_service.savedChats() as { id: string }).id;
+    this.chat_service
+      .deleteChat(id)
+      .then(() => {
+        let currentUrl = this.router.url;
+        this.dismiss.set(true);
+
+        this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+          this.router.navigate([currentUrl]);
+        })
+      })
+      .catch((err) => alert(err.message));
+    this.chat_service.listChat();
+  }
 }

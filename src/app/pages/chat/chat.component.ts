1fd22e43ef142416cb4fefb620768663e70af669
Manoj <manoj09633@gmail.com>
2024-10-21 11:04:13 +0530

    implemented chat service in chat component

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,21 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { ChatService } from '../../supabase/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
 
   private auth = inject(AuthService);
-  private router = inject(Router)
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+  private chat_service = inject(ChatService);
+  chatForm!: FormGroup;
+
+  constructor() {
+    this.chatForm = this.fb.group({
+      chat_message: ['', Validators.required]
+    })
+  }
+
   async logout() {
     this.auth.signOut().then(() => {
       this.router.navigate(['/login'])
+    }).catch((err) => {
+      alert(err.message);
+    })
+  }
+
+  onSubmit() {
+    const formValue = this.chatForm.value.chat_message;
+    console.log(formValue)
+
+    this.chat_service.chatMessage(formValue).then((res) => {
+      console.log(res)
     }).catch((err) => {
       alert(err.message);
     })
